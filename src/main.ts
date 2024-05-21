7bfffc935a6d7db87370439a368d4b946cee8452
StarPandaBeg <kshmurakov@mail.ru>
2024-05-21 22:07:45 +0300

    feat: setup swagger to generate openapi docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('nest-auth-template')
+    .setDescription('Шаблон NestJS приложения со встроенной авторизацией')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
